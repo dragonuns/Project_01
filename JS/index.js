4f815a8e57cb9b8b2860a61cecbef8914094c2f5
Mr.Combo <1459074125@qq.com>
2023-02-04 17:10:05 +0800

    已提交

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,17 @@
 
 const fs = require('fs');
 
-fs.writeFile("./1.txt",'Hello!node.js',function(err,result){
+// fs.writeFile("./成绩.txt",'Hello!node.js',function(err,result){
+//     if(err){
+//         console.log("写入文件失败：\n"+err.mesage);
+//     }
+//     console.log("写入文件成功!" );
+// })
+fs.readFile("./成绩.txt",'utf8',function(err,result){
     if(err){
-        console.log("写入文件失败：\n"+err.mesage);
+        console.log("读取文件失败!\n"+err.mesage);
     }
-    console.log("写入文件成功!" );
-})
-fs.readFile("./1.txt",'utf8',function(err,result){
-    if(err){
-        console.log("读取文件失败：\n"+err.mesage);
-    }
-    console.log("读取文件成功：\n" + result);
+    console.log("读取文件成功!\n" );
+    const arrOld = result.split('');
+    console.log(arrOld);
 })
