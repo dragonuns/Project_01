d8a9e1d3a40c87c86c7b7a2b9593ac7ea68a295b
Mr.Combo <1459074125@qq.com>
2023-02-04 16:47:06 +0800

    已提交

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,12 @@
 
 const fs = require('fs');
+
+fs.writeFile("./1.txt",'Hello!node.js',function(err,result){
+    if(err){
+        console.log("写入文件失败：\n"+err.mesage);
+    }
+    console.log("写入文件成功!" );
+})
 fs.readFile("./1.txt",'utf8',function(err,result){
     if(err){
         console.log("读取文件失败：\n"+err.mesage);
