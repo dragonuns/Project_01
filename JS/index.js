c28b1beab9b4d7641677f7b759b8e1d579a90f7f
Mr.Combo <1459074125@qq.com>
2023-02-04 16:24:50 +0800

    已提交

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,8 @@
-$(function(){
+
 const fs = require('fs');
 fs.readFile("./1.txt",'utf8',function(err,result){
     if(err){
         console.log(err.mesage);
     }
     console.log(result);
-})
-
-
 })
