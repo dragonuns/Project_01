6b69291a03eb9af3df5977fa05545f70f9b82a5e
Mr.Combo <1459074125@qq.com>
2023-02-04 18:05:32 +0800

    已提交

--- JS/整理成绩.js
+++ JS/整理成绩.js
@@ -1,15 +1,27 @@
 
 const fs = require('fs');
-fs.readFile("./成绩.txt",'utf8',function(err,result){
+fs.readFile(__dirname+"/成绩.txt",'utf8',function(err,result){
+    // console.log(err);
     if(err){
         console.log("读取文件失败!\n"+err.mesage);
     }
-    console.log("读取文件成功!" );
+    else{
+          console.log("读取文件成功!" );
+    }
+  
    const arrOld = result.split(" ");
    const arrNew = [];
    arrOld.forEach(item=>{
     arrNew.push(item.replace("=",":"))
+    
    })
-   const newStr = arrNew.join("\n");
-   console.log(newStr);
+const newStr = arrNew.join("\n");
+
+fs.writeFile(__dirname+"/成绩 - ok.txt",newStr,function(err,result){
+    if(err){
+        console.log("写入文件失败!\n"+err.mesage);
+    }
+    console.log("写入文件成功!" );
+   
+ })
 })
