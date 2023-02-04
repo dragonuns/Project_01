c22d0a6ec73715df694117cc3c51d5b89721fa04
Mr.Combo <1459074125@qq.com>
2023-02-04 16:36:27 +0800

    已提交

--- JS/index.js
+++ JS/index.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 fs.readFile("./1.txt",'utf8',function(err,result){
     if(err){
-        console.log(err.mesage);
+        console.log("读取文件失败：\n"+err.mesage);
     }
     console.log("读取文件成功：\n" + result);
 })
