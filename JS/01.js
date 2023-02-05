7e2e29b2ac65cfc336997d978d6e20b748949572
Mr.Combo <1459074125@qq.com>
2023-02-06 00:08:15 +0800

    已提交

--- JS/01.js
+++ JS/01.js
@@ -5,9 +5,9 @@ module.exports = {
 height:175,
 weight:120,
 }
-module.exports.age = age;
-module.exports.uname = uname;
-module.exports = {
+exports.age = age;
+exports.uname = uname;
+exports = {
    food:"番茄",
    hatefood:"海鲜",
     }
--- JS/02.js
+++ JS/02.js
@@ -1,2 +1,3 @@
 const m1 = require("./01");
 console.log(m1);
+console.log(exports === module.exports);
