3ddef44abd3f547ea98e4069297785deb93e35a6
Mr.Combo <1459074125@qq.com>
2023-02-06 00:18:07 +0800

    已提交

--- JS/01.js
+++ JS/01.js
@@ -1,12 +1,12 @@
 
 const age = 20;
 const uname = "zs"
-module.exports = {
+exports = {
 height:175,
 weight:120,
 }
-exports.age = age;
-exports.uname = uname;
+module.exports.age = age;
+module.exports.uname = uname;
 exports = {
    food:"番茄",
    hatefood:"海鲜",
